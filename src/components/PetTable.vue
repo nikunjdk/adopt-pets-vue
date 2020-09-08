afa1991af6b38ae1436103db377db17a6fbfe943
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2020-09-08 13:21:11 +0530

    Initial Commit

---
+++ src/components/PetTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{species | capitalize}} For Adoption</h1>
+    <b-table striped hover :items="pets">
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/${species}/${data.index}`">{{ data.value }}</router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    species: String,
+    pets: Array
+  },
+  filters: {
+    capitalize: function (value) {
+      console.log(value.charAt(0).toUpperCase() + value.slice(1))
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,164 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view-container">
+    <h1>Adopt A New best Friend</h1>
+    <p>Number of Animals Up for Adoption: {{ animalsCount }}</p>
+    <button @click="togglePetForm" class="btn btn-primary">Add A New Pet</button>
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+
+      <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="formData.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Species:" label-for="input-2">
+        <b-form-select
+          id="input-2"
+          v-model="formData.species"
+          :options="['cat','dog']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Age:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="formData.age"
+          type="number"
+          required
+          placeholder="Enter Age"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Breed:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="formData.breed"
+          required
+          placeholder="Enter Breed"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-5" label="Gender:" label-for="input-5">
+        <b-form-select
+          id="input-5"
+          v-model="formData.gender"
+          :options="['male','female']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-6" label="Color:" label-for="input-6">
+        <b-form-input
+          id="input-6"
+          v-model="formData.color"
+          required
+          placeholder="Enter Color"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-7" label="Weight:" label-for="input-7">
+        <b-form-input
+          id="input-7"
+          v-model="formData.weight"
+          type="number"
+          required
+          placeholder="Enter Weight"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-8" label="Location:" label-for="input-8">
+        <b-form-input
+          id="input-8"
+          v-model="formData.location"
+          required
+          placeholder="Enter Location"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-9" label="Notes:" label-for="input-9">
+        <b-form-input
+          id="input-9"
+          v-model="formData.notes"
+          required
+          placeholder="Enter Notes"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapActions, mapGetters } from 'vuex'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null,
+        breed: '',
+        gender: null,
+        color: '',
+        weight: 0,
+        location: '',
+        notes: ''
+      }
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'animalsCount'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    handleSubmit () {
+      const { species, age, name, breed, gender, color, weight, location, notes } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age,
+          breed,
+          gender,
+          color,
+          weight,
+          location,
+          notes
+        }
+      }
+      this.addPet(payload)
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null,
+        breed: '',
+        gender: null,
+        color: '',
+        weight: 0,
+        location: '',
+        notes: ''
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+  .btn {
+    margin: 0px 5px;
+  }
+</style>
